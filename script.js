9e881b883392feb55dbb0104c315e2f3521bea36
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2020-08-23 01:36:38 +0700

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 
-var character_width = 22;
-var character_height = 50;
+var character_width = 42;
+var character_height = 100;
 
 var obstacle_width = 40;
 var obstacle_height = 40;
@@ -26,7 +26,7 @@ var checkDead = setInterval(function () {
     let characterBottomPoint = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
     let obstacleLeftPoint = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
 
-    if (obstacleLeftPoint < character_width && obstacleLeftPoint > (-obstacle_width) && characterBottomPoint <= obstacle_height) {
+    if (obstacleLeftPoint < character_width * 0.8 && obstacleLeftPoint > (-obstacle_width * 0.8) && characterBottomPoint <= obstacle_height * 0.8) {
         obstacle.style.animation = "none";
         alert("Game Over. Score: " + Math.floor(counter / 100));
         counter = 0;
